cd8371cfe55692815e968737185a172f15adc0e1
Elijah <elijahsharper1@gmail.com>
2018-01-26 23:50:02 +0300

    Timeline VueComponent Setup and User avatar attribute

---
+++ resources/assets/js/components/Timeline.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Timeline</div>
+
+                    <div class="panel-body">
+                        <post v-for="post in posts" :post="post" :key="post.id"></post>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Post from './Post.vue';
+
+    export default {
+        components: {
+          Post
+        },
+
+        data() {
+          return {
+            posts: []
+          }
+        },
+
+        mounted() {
+            this.$http.get('/posts').then((response) => {
+              this.posts = response.data
+              // console.log(response.data);
+            })
+        }
+    }
+</script>
