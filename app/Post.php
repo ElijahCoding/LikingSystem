91b5eb5615f3800162b11c6881d382d3215c4c70
Elijah <elijahsharper1@gmail.com>
2018-01-27 00:17:25 +0300

    Post-Like polymorphic relationship

--- app/Post.php
+++ app/Post.php
@@ -2,11 +2,14 @@
 
 namespace App;
 
+use Auth;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     protected $guarded = [];
+
+    protected $appends = ['likeCount', 'likedByCurrentUser'];
 
     public function user()
     {
@@ -16,5 +19,24 @@ class Post extends Model
     public function scopeLatestFirst($builder)
     {
       return $builder->orderBy('created_at', 'desc');
+    }
+
+    public function getLikeCountAttribute()
+    {
+      return $this->likes->count();
+    }
+
+    public function getLikedByCurrentUserAttribute()
+    {
+      if (!Auth::check()) {
+        return false;
+      }
+
+      return $this->likes->where('user_id', auth()->id())->count() === 1;
+    }
+
+    public function likes()
+    {
+      return $this->morphMany(Like::class, 'likeable');
     }
 }
